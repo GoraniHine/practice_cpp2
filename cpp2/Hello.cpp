7ed72302418ac0d50bfde86527681067d8bfc553
MINSCHOI <128362719+GoraniHine@users.noreply.github.com>
2025-10-19 17:03:43 +0900

    Add files via upload

---
+++ cpp2/Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // 전처리 지시자
+
+using namespace std; // std:: 생략
+// ';' 종결자의 역할을 한다.
+
+int main()
+{
+	// c++ 코드에는 반드시 main이라는 함수가 있어야 한다.
+
+	cout << "Hello, World!" << endl;
+	// << 꺽쇠는 데이터의 방향을 의미함.
+
+	return 0;
+}
