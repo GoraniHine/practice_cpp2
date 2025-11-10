a8e3837015bb23712548c651d2bb39df54e69227
MINSCHOI <128362719+GoraniHine@users.noreply.github.com>
2025-11-10 22:31:17 +0900

    Add files via upload

---
+++ B10828/B10828.cpp
@@ -0,0 +1,36 @@
+#include <iostream> // STL = Standard Template Library
+#include <queue>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, x;
+	cin >> N;
+	priority_queue<int> pq;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x;
+		if (x == 0)
+		{
+			if (pq.empty())
+			{
+				cout << 0 << '\n';
+			}
+			else
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(x);
+		}
+	}
+}
